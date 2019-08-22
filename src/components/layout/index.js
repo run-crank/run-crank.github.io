2f4cb8e1e3c75b2fc6f78462743ab3738ad7085d
Eric Peterson <i.am@eric.pe>
2019-08-22 12:12:06 -0700

    Add GA tracking.

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -9,7 +9,17 @@ import './_layout.scss'
 
 const TemplateWrapper = ({ children, activeTrail, subTitle, invertHeaderColors }) => (
     <div className="app-wrapper">
-        <Helmet title={"Crank - " + (subTitle || 'BDD Test Automation for Integrated SaaS')} />
+        <Helmet title={"Crank - " + (subTitle || 'BDD Test Automation for Integrated SaaS')}>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-75228722-5"></script>
+            <script>{`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'UA-75228722-5');
+            `}</script>
+        </Helmet>
         <Header invertColors={invertHeaderColors} activeTrail={activeTrail} />
         {children}
         <Footer />
